d1b36eac752109d7034c75b370619d29ca4fc7c4
Albert Shen <albertshen1206@gmail.com>
2015-08-28 17:38:18 +0800

    add video server

---
+++ video_server/conf/config.php
@@ -0,0 +1,21 @@
+<?php
+
+define("HOST","http://127.0.0.1:8080");
+
+define("AVCONV", "/usr/bin/avconv");
+define("TEMPLATES", dirname(__FILE__) . '/../templates');
+define("GENERATE", dirname(__FILE__) . '/../generate');
+define("CACHE", dirname(__FILE__) . '/../cache');
+
+
+define("SCALE", '404:720');
+define("VRATE", '600k');
+define("ARATE", '48k');
+
+
+function __autoload($class_name) {
+    if(file_exists(dirname(__FILE__) . '/../lib/' . $class_name . '.php')) 
+    	require_once dirname(__FILE__) . '/../lib/' . $class_name . '.php';
+}
+
+?>

46fc216dc8a1f7332cc3422f08971ca288ac79c5
Demon Zhang <demon.zhang@samesamechina.com>
2015-09-17 14:40:41 +0800

    no message

--- src/Givenchy/EyelipBundle/Services/Image/ImageService.php
+++ src/Givenchy/EyelipBundle/Services/Image/ImageService.php
@@ -94,13 +94,15 @@ class ImageService
         //相框合成到背景
         imagecopyresized($bg,$bgtpl,0,0,0,0,$widthbg,$heightbg,$widthtpl,$heighttpl); 
 
+        $image=imagecreatetruecolor($w, $h);
+        imagecopyresized($image,$bg,0,0,0,0,448,720,$widthbg,$heightbg); 
         //生成图片
         $fs = new Filesystem();
         if(!$fs->exists($this->_filedir . '/VideoPic'))
            $fs->mkdir($this->_filedir . '/VideoPic', 0700);
         $fileName = '/VideoPic/' . time() . rand(100,999) . '.png';
         $hechengImg = $this->_filedir . $fileName;
-        ImagePng($bg,$hechengImg);
+        ImagePng($image,$hechengImg);
         return $hechengImg;
     }
 

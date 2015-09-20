bb07b12ef74fd410fde9e2b638ed3054f9ba3658
Demon Zhang <demon.zhang@samesamechina.com>
2015-09-20 12:55:40 +0800

    no message

--- src/Givenchy/EyelipBundle/Services/User/UserService.php
+++ src/Givenchy/EyelipBundle/Services/User/UserService.php
@@ -168,14 +168,15 @@ class UserService
             $_COOKIE['eyelip_uuid'] = $uuid;
         }
         $log = $this->em->getRepository('GivenchyEyelipBundle:Likelog')
-            ->findOneBy(array('uuid' => $_COOKIE['eyelip_uuid'], 'videoId' => $id));
+            ->findOneBy(array('uuid' => $_COOKIE['eyelip_uuid'], 'videoId' => $id, 'created' => date('Ymd')));
         if ($log) {
             return -1;
         }
+
         $likelog = new Likelog();
         $likelog->setUuid($_COOKIE['eyelip_uuid']);
         $likelog->setVideoId($id);
-        $likelog->setCreated(time());
+        $likelog->setCreated(date("Ymd"));
         $this->save($likelog);
         $video = $this->em->getRepository('GivenchyEyelipBundle:Video')
             ->findOneBy(array('id' => $id));

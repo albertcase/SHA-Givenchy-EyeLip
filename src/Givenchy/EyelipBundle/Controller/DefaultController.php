7d37444323aa4d2f62afd3f06502988720be6ef0
Demon Zhang <demon.zhang@samesamechina.com>
2015-09-28 11:49:27 +0800

    no message

--- src/Givenchy/EyelipBundle/Controller/DefaultController.php
+++ src/Givenchy/EyelipBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Givenchy\EyelipBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Givenchy\EyelipBundle\Entity\Offline;
 
 class DefaultController extends Controller
 {
@@ -14,56 +15,60 @@ class DefaultController extends Controller
 
     public function testAction()
     {
-  //   	$uri = "http://vcn.ts-online.com.tw/handle_vote.php";
-  //   	for($i=0;$i<150;$i++){
-		// 	$data = array(
-		// 	     'cid' => '24' ,
-		// 	     'uid' => '0019'.rand(100,999) ,
-		// 	     'pid' => '8'
-		// 	   );
-			 
-		// 	$ch = curl_init ();
-		// 	curl_setopt ( $ch, CURLOPT_URL, $uri . "?" . http_build_query($data) );
-		// 	curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		// 	$return = curl_exec ( $ch );
-		// 	curl_close ( $ch );
-		// 	echo $i."<br>";
-		// }
-
-		$uri = "http://127.0.0.1:8080/index.php";
-		$data = array(
-                'images' => array(
-                        base64_encode(file_get_contents('/vagrant/web/4.png')),
-                        base64_encode(file_get_contents('/vagrant/web/5.png')),
-                        base64_encode(file_get_contents('/vagrant/web/6.png')),
-                        ),
-                'video_tpl' => 'vone',
-                'audio_tpl' => 'aone',
-                 );
+  		//检查
+  		$csv = 'files/mobile.csv';
+		$handle = fopen($csv,"r");
+		$total=0;
+		$ok=0;
+		while(!feof($handle)){
+			$line = fgets($handle,4096);
+			if($line==''){
+				continue;
+			}
+			$total++;
+			$offline = new Offline();
+			$offline->setMobile($line);
+			$offline->setStatus(0);
+			$offline->setCreated(date('Ymd'));
+			$ok++;
+		}
+		fclose($handle);
+		echo $total . "--" . $ok;
+		exit;
+		// $uri = "http://127.0.0.1:8080/index.php";
+		// $data = array(
+  //               'images' => array(
+  //                       base64_encode(file_get_contents('/vagrant/web/4.png')),
+  //                       base64_encode(file_get_contents('/vagrant/web/5.png')),
+  //                       base64_encode(file_get_contents('/vagrant/web/6.png')),
+  //                       ),
+  //               'video_tpl' => 'vone',
+  //               'audio_tpl' => 'aone',
+  //                );
 		 
-		$ch = curl_init ();
-		curl_setopt ( $ch, CURLOPT_URL, $uri);
-		curl_setopt ( $ch, CURLOPT_POST, 1 );
-		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
-		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt ( $ch, CURLOPT_POSTFIELDS, http_build_query($data) );
-		echo $return = curl_exec ( $ch );
-		curl_close ( $ch );
-    	exit;
-    	$uri = "http://sms.bechtech.cn/Api/send/data/json";
-		$data = array(
-		     'accesskey' => '3619' ,
-		     'secretkey' => 'a8040842375976cd4009452c8348609a6151ff41' ,
-		     'mobile' => '13524703157' ,
-		     'content' => urlencode('亲，您之前参与的纪梵希魅影朱唇活动，请猛戳测试英文test测试中文符！@＃¥％……&＊（）发送进行集赞数兑换！退订回复TD【纪梵希】') 
-		);
+		// $ch = curl_init ();
+		// curl_setopt ( $ch, CURLOPT_URL, $uri);
+		// curl_setopt ( $ch, CURLOPT_POST, 1 );
+		// curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+		// curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		// curl_setopt ( $ch, CURLOPT_POSTFIELDS, http_build_query($data) );
+		// echo $return = curl_exec ( $ch );
+		// curl_close ( $ch );
+  //   	exit;
+  //   	$uri = "http://sms.bechtech.cn/Api/send/data/json";
+		// $data = array(
+		//      'accesskey' => '3619' ,
+		//      'secretkey' => 'a8040842375976cd4009452c8348609a6151ff41' ,
+		//      'mobile' => '13524703157' ,
+		//      'content' => urlencode('亲，您之前参与的纪梵希魅影朱唇活动，请猛戳测试英文test测试中文符！@＃¥％……&＊（）发送进行集赞数兑换！退订回复TD【纪梵希】') 
+		// );
 		 
-		$ch = curl_init ();
-		curl_setopt ( $ch, CURLOPT_URL, $uri . "?" . http_build_query($data) );
-		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		$return = curl_exec ( $ch );
-		curl_close ( $ch );
-		var_dump($return);
+		// $ch = curl_init ();
+		// curl_setopt ( $ch, CURLOPT_URL, $uri . "?" . http_build_query($data) );
+		// curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		// $return = curl_exec ( $ch );
+		// curl_close ( $ch );
+		// var_dump($return);
         exit;
     }
 }
